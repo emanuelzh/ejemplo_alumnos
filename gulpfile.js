ec48c2f1a3dda9a8aae7c03ba79e222882ec3c51
emanuelzh <emanuelz@emanuelz.com.mx>
2015-08-27 10:12:10 -0500

    fix da mess con gulp y el front-end

--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,23 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
- //mezcla el css
- mix.styles([
-     'bower_components/bootstrap/dist/css/bootstrap.min.css',
-     'main.css'
- ],'public/css/main.css','resources/assets');
+    //mezcla el css (sin fucking bootswatch bootstrap
+    mix.styles([
+        'bower_components/font-awesome/css/font-awesome.min.css',
+        'main.css'
+    ],'public/css/main.css','resources/assets');
 
- //mezcla los scripts
- mix.scripts([
-     'bower_components/jquery/dist/jquery.min.js',
-     'bower_components/bootbox.js/bootbox.js',
-     'main.js'
- ],'public/js/main.js','resources/assets');
+    //mezcla los scripts
+    mix.scripts([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootbox.js/bootbox.js',
+        'main.js'
+    ],'public/js/main.js','resources/assets');
+
+    //copiar las fuentes para fontawesome
+    mix.copy('resources/assets/bower_components/font-awesome/fonts','public/fonts');
+
+    //copia el bootstrap
+    mix.copy('resources/assets/bower_components/bootswatch/flatly/bootstrap.min.css','public/css/bootstrap.min.css');
 
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <title>Skilltest</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="row">
+                <div class="col-md-12 col-lg-12">
+                    <h2>Alumnos</h2>
+                    <hr>
+                    
+                </div>
             </div>
         </div>
+    <script language="javascript" src="{{asset('js/main.js')}}"></script>
     </body>
 </html>
